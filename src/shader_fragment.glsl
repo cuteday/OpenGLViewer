7be87da9d1626810e78e2f1b500210fb23b1dd33
融雪时分 <cutedays@icloud.com>
2020-11-30 19:56:44 +0800

    Basic codes

---
+++ src/shader_fragment.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec4 Color;
+
+uniform sampler2D texture_diffuse1;
+
+void main()
+{    
+	FragColor = Color;
+    // FragColor = texture(texture_diffuse1, TexCoords);
+}
